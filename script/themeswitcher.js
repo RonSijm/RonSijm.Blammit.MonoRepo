04961bede4aa60a1040bb2957e4d638fd38dbbda
RonSijm <RonSijm@users.noreply.github.com>
2023-10-10 18:14:24 +0000

    Deploying to gh-pages from @ RonSijm/RonSijm.Blammit.MonoRepo@be5dd8ed8b848ba8d8bfe6afb1c01c404dd1d885 🚀

---
+++ script/themeswitcher.js
@@ -0,0 +1,16 @@
+﻿var getBodyElement = function () {
+    return document.getElementsByTagName("body")[0];
+}
+var addCssClassToBody = function (cssClass) {
+    var body = getBodyElement();
+
+    if (!body.classList.contains(cssClass)) {
+        body.classList.add(cssClass);
+    }
+}
+
+var removeCssClassFromBody = function (cssClass) {
+    var body = getBodyElement();
+    if (body.classList.contains(cssClass))
+        body.classList.remove(cssClass);
+}
